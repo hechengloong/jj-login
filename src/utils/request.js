f8b729378fcc1cc515407bf29d0f3890df0edf31
hechenglong <1030110207@qq.com>
2025-09-26 14:41:29 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,7 +16,7 @@ const defaultOptions = {
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
     'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
   },
 };
 
